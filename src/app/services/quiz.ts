8fd00b6e25244aa24958a8c70a63117b2b9bb90c
arrogante93 <andresmauriciovillacobgarrido@hotmail.com>
2026-05-03 18:16:22 -0500

    Configure environments and update QuizService to use production API URL

--- src/app/services/quiz.ts
+++ src/app/services/quiz.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 export interface Question {
   question: string;
@@ -18,7 +19,7 @@ export interface Quiz {
   providedIn: 'root'
 })
 export class QuizService {
-  private apiUrl = 'http://localhost:8080/home';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
